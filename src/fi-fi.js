c77a1403eb4c931996c59add7a8440d50aa5cd04
Niko Koivu <niko.koivu@gmail.com>
2013-08-06 00:12:04 +0300

    getcm: FI translations
    Change-Id: I89bc8b1b72432e21d4cb9e18aae4bc37663dbb95

---
+++ src/fi-fi.js
@@ -0,0 +1,17 @@
+(function() {
+  var getcm = window.getcm || { locales:{} };
+
+  _.extend(getcm.locales, { "fi-FI": {
+    navbar: {
+      about:"Tietoja",
+      download:"Lataa",
+      devices:"Laitteet",
+      blog:"Blogi",
+      forum:"Foorumi",
+      community:"Yhteisö",
+      manufacturers:"Valmistajat",
+      all_manufacturers:"Kaikki valmistajat"
+    }
+  }});
+
+}).call(this);
