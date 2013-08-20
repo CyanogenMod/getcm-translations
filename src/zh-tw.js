2a565201677a7726c07f20ea2b72cd9054918718
Chi Tung Li <tonyli1357@gmail.com>
2013-08-20 20:17:46 +0800

    GetCM: zh-TW translations
    Change-Id: I012202b410b9712e185e03eadd59fc33781c1ac1

---
+++ src/zh-tw.js
@@ -0,0 +1,17 @@
+(function() {
+  var getcm = window.getcm || { locales:{} };
+
+  _.extend(getcm.locales, { "zh-TW": {
+    navbar: {
+      about:"關於",
+      download:"下載",
+      devices:"裝置",
+      blog:"部落格",
+      forum:"論壇",
+      community:"社群",
+      manufacturers:"製造商",
+      all_manufacturers:"所有製造商"
+    }
+  }});
+
+}).call(this);
