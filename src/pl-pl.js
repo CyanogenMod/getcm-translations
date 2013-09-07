48bc00b5327f01f2fc4c5fc2ad653dc0bb2a7cb9
Tomasz Popielarczyk <popielarczyktomasz@gmail.com>
2013-09-07 15:28:55 +0200

    GetCM: Add Polish translation
    Change-Id: I99736841264805813c1e6a026c2cc7ccc2536b66

---
+++ src/pl-pl.js
@@ -0,0 +1,17 @@
+(function() {
+  var getcm = window.getcm || { locales:{} };
+
+  _.extend(getcm.locales, { "pl-PL": {
+    navbar: {
+      about:"O projekcie",
+      download:"Pobierz",
+      devices:"Urządzenia",
+      blog:"Blog",
+      forum:"Forum",
+      community:"Społeczność",
+      manufacturers:"Producenci",
+      all_manufacturers:"Wszyscy producenci"
+    }
+  }});
+
+}).call(this);
